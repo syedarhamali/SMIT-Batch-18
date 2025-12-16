54e556e1ae96e2baf8e0010fcb2502dfc8521b74
Arham <arhamali63@gmail.com>
2025-12-16 16:45:16 +0500

    if else statement

---
+++ js-class-6/index.js
@@ -0,0 +1,25 @@
+// var havingCNIC = prompt("do you have CNIC??")
+
+
+
+// if(havingCNIC == "yes"){
+//     alert("you are eligible for vote!!")
+// } else{
+//     alert("bachy ho abhi vote nh de sakty!!")
+// }
+
+
+// = (is used to assignment )
+// == (is used to compare values) 1 == '1' (true)
+// === (is used to compare values with data type) 1 === 1 (true)
+
+
+var yourNumber = prompt("Enter your number!")
+
+
+if (yourNumber != 3) {
+    console.log("you passed!")
+} else {
+    console.log("you are top scorer!")
+}
+ 
