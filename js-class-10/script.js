8ffd5aba49dd8c49218c8a30ecd4e0bfcb90e2b8
Ashrib <asahrib920@gmail.com>
2026-01-01 16:02:25 +0500

    for loop

---
+++ js-class-10/script.js
@@ -0,0 +1,16 @@
+// debugger
+
+//     // initialization; condition ; increment/decrement 
+// for (var i = 1; i < 10; i++ ){
+//     document.write("Hello" + i + "<br />")
+// }
+
+
+
+
+
+var userInput = prompt("konsa table print krwana chatae ho")
+
+for (var i= 1 ; i<= 10 ; i = i + 2){
+    document.write(userInput + " x " + i + " = " + userInput * i +  '<br />')
+}
