377e787d6654b22acfc9b5e3a32e25fec16f57a4
Arham <arhamali63@gmail.com>
2026-01-03 15:53:44 +0500

    changing case

--- js-class-10/script.js
+++ js-class-10/script.js
@@ -9,8 +9,26 @@
 
 
 
-var userInput = prompt("konsa table print krwana chatae ho")
+// var userInput = prompt("konsa table print krwana chatae ho")
 
-for (var i= 1 ; i<= 10 ; i = i + 2){
-    document.write(userInput + " x " + i + " = " + userInput * i +  '<br />')
-}
+// for (var i= 1 ; i<= 10 ; i++){
+//     document.write(userInput + " x " + i + " = " + userInput * i +  '<br />')
+// }
+
+
+// var cities = ["Karachi", "Lahore", "Islamabad", "Peshawar", "Quetta", "Rawalpindi"]
+// var userCity = prompt("Apka sheher konsa he??")
+
+// var toSmallLetters = userCity.toLowerCase()
+
+// for (i = 0; i < cities.length; i++) {
+//     if (cities[i].toLowerCase() === toSmallLetters) {
+//         console.log(cities[i] + ' is the cleaniest city of pakistan')
+//        break;
+//     }
+// }
+
+
+// var cityToCheck = 'Islamabad'
+// var firstChar = cityToCheck.slice(0, 5);
+// console.log(firstChar)
