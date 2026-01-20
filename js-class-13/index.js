d6df38f8ff5da855c1794a997332c3db0794c96f
Arham <arhamali63@gmail.com>
2026-01-20 15:48:15 +0500

    date objec

---
+++ js-class-13/index.js
@@ -0,0 +1,57 @@
+// // console.log(new Date())
+
+
+// // function greet(personName = 'User') {
+// //     console.log("welcome to my website " + personName)
+// // }
+
+// // greet("Arham")
+
+
+// // function getSumOfTwoNumbers(numOne , numTwo){
+// //     var result = numOne + numTwo 
+// //     return result
+// // }
+
+
+// // var output = getSumOfTwoNumbers(10,20)
+// // console.log(output)
+
+
+
+// var myDate = new Date()
+
+// console.log(myDate)
+
+// var today = new Date()
+// console.log(today)
+
+// today.getDay()
+
+// today.getFullYear()
+
+// today.getHours()
+
+// today.getMinutes()
+
+// new Date().getMinutes()
+
+// new Date().getMilliseconds()
+
+// new Date()
+
+// new Date().toString()
+
+// new Date('January 30 2026')
+
+// new Date('January 30 2030')
+
+// var today = new Date()
+
+// var wahajDOB = new Date('December 23 2008')
+
+// today - wahajDOB
+
+// 1000 * 60 *60*24
+
+// 538931567351 / 86400000
