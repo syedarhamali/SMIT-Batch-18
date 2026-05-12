f9d902a570c6859623f60b0a85442e8aaa9d4703
manaheeljamil <manaheeljamil20@gmail.com>
2026-05-12 16:02:39 +0500

    throw work

---
+++ js-class-36/index.js
@@ -0,0 +1,20 @@
+function getData(){
+    try{
+        console.log(developerName)
+        // const response = await fetch('https://dummyjsosss.com/products')
+        // const {products} = await response.json()
+        // console.log(products)
+    }catch(error){
+        console.log(error)
+        throw "Api Endpoint me msla he.";
+        console.log(error)
+        console.log(error)
+        console.log(error)
+        console.log(error)
+    } 
+    console.log("function chal raha he last tk")
+}
+
+getData()
+
+console.log("running")
