e1baed1298075c4236a70b6d4b4752b491fc36f5
Ashrib <asharib920@gmail.com>
2026-05-16 15:23:11 +0500

    call,bind and apply

---
+++ js-class-37/app.js
@@ -0,0 +1,128 @@
+// console.log(this)
+
+// const user = {
+//     name: 'Arham',
+//     gender: 'Male',
+//     phoneNumber: 90078601,
+//     getDetails: function (){
+//         console.log(this.name )
+//         console.log(this.phoneNumber)
+//         console.log(this.gender)
+//         console.log(this.nationality)
+//     }
+// }
+
+// user.getDetails()
+
+// const user1 = {
+//     name: 'Arham',
+//     gender: 'Male',
+//     phoneNumber: 90078601,
+
+// }
+
+// const user2 = {
+//     name: 'Ahmed',
+//     gender: 'Male',
+//     phoneNumber: 90078602,
+// }
+
+// function getDetails() {
+//     console.log('this is object name value', this.name)
+//     console.log('this is object gender value', this.gender)
+//     console.log('this is object phoneNumber value', this.phoneNumber)
+// }
+
+// getDetails.call(user1)
+// getDetails.call(user2)
+// getDetails.call(user2)
+
+
+// const arham = {
+//     balance: 20000,
+//     name: 'Arham Ali',
+//     bank: 'Meezan'
+// }
+
+// const ahmed= {
+//      balance: 20000,
+//     name: 'Ahmed',
+//     bank: 'Meezan'
+// }
+
+// // Call 
+// function getDetail(city , country){
+
+//     console.log(this.balance)
+//     console.log(this.name)
+//     console.log(this.bank)
+//     console.log(city)
+//     console.log(country)
+
+// }
+
+// getDetail.call(arham , 'Karachi' , 'Pakistan')
+// getDetail.call(ahmed , 'Multan' , 'Pakistan')
+
+
+// // apply 
+
+// const arham = {
+//     balance: 20000,
+//     name: 'Arham Ali',
+//     bank: 'Meezan'
+// }
+
+// const ahmed= {
+//      balance: 20000,
+//     name: 'Ahmed',
+//     bank: 'Meezan'
+// }
+
+// // Apply 
+// function getDetail(city , country){
+
+//     console.log(this.balance)
+//     console.log(this.name)
+//     console.log(this.bank)
+//     console.log(city)
+//     console.log(country)
+
+// }
+
+// getDetail.apply(arham , ['Karachi' , 'Pakistan'])
+
+
+
+// const user = {
+//     name : 'Arham',
+//     greet: function (){
+//         console.log(name)
+//     }
+// }
+
+// function myDetails(){
+//     var personName = 'Arham'
+
+//     function getName(){
+//         console.log(personName)
+//     }
+
+//     return getName
+// }
+
+// const getDetail = myDetails()
+// getDetail()
+
+
+// Bind 
+const userOne = {
+    personName: 'Arham Ali',
+    greet: function (){
+        console.log(this.personName)
+    }
+}
+
+var getDetail = userOne.greet.bind(userOne)
+
+getDetail()
